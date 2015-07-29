a7f80356c74f2fbee9ec5b15e58386ea0b246195
zhouyu5460 <zhouyu5460@gmail.com>
2015-07-29 16:18:42 +0800

    0.1.2

--- imu.py
+++ imu.py
@@ -80,22 +80,20 @@ def imu(acc,gyro):
   return (pitch, roll, yaw, v_acc)
 
 speed = 0.0
-distance = 0.0
 pre_tm = 0.0
 sum_t = 0.0
 
-def calc_height(v_acc):
+def calc_speed(v_acc):
     global speed, distance, pre_tm, sum_t
     if (pre_tm-0.0001 < 0):
         pre_tm = timeit.default_timer() 
     else:
         tm_sec =  timeit.default_timer() - pre_tm
         
-        speed = (9.8 * (v_acc-0.9998))*tm_sec + speed;
-        distance += speed*tm_sec;
+        speed = (9.8 * (v_acc-1))*tm_sec + speed;
         sum_t += tm_sec
         pre_tm = timeit.default_timer()
-    return distance,sum_t,speed
+    return sum_t,speed
     
     
 cnt = 0
@@ -172,10 +170,12 @@ def key_task():
  
 
 d_vel_tm_buf =  deque()
+
+
 def calc_task():
     sample_cnt = 0
     sum_vcc = 0
-    global d_acc_gyr, pre_tm
+    global d_acc_gyr, pre_tm, speed,sum_t
     cali_cnt = 0
     cali_ax = 0.0
     cali_ay = 0.0
@@ -184,7 +184,18 @@ def calc_task():
     cali_gx = 0.0
     cali_gy = 0.0
     cali_gz = 0.0
-    fh = open("res.txt", "w")
+    
+    
+    st_move = 0
+    pre_st_move = 0
+    
+    start_cnt = 0
+    end_cnt = 0
+    data_st = 0
+    calc_st = 0
+    dis = 0.0
+    fd2 = open("origin.txt", "w")
+    
     while 1:
         
         if len(d_acc_gyr) != 0:
@@ -207,24 +218,58 @@ def calc_task():
             #print "gyro:", gx, gy, gz
             pry = imu([ax,ay,az],[gx,gy,gz])
             
-            h,t,v =  calc_height(pry[3])
-            
-            d_vel_tm_buf.append([t,v])
-            l_vel = []
-            if (gx < 15 and gy < 15 and gz < 15 and len(d_vel_tm_buf) >100 ):
-
-                a = d_vel_tm_buf[-1][1]/ d_vel_tm_buf[0][-1]
-                last_tm = 0.0
-                for val in d_vel_tm_buf:            
-                    l_vel.append([val[0] - last_tm , val[1] - val[1]*a])
-                    last_tm = val[0]
-                for val in l_vel:
-                    dis += val[1]*val[0]
-                d_vel_tm_buf.clear()
-                print dis
-                    
-            
             #print "pitch:%f roll:%f yaw:%f v_acc:%f" %(pry[0],pry[1],pry[2],pry[3])
+            if (abs(gx) <= 50): #and abs(gy) < 15 and abs(gz) < 15
+                end_cnt += 1
+                start_cnt = 0
+                if end_cnt > 100:
+                    pre_st_move = st_move
+                    st_move = 0 
+            else:
+                start_cnt += 1
+                end_cnt = 0
+                if start_cnt > 50:
+                    pre_st_move = st_move
+                    st_move = 1
+            if 0 == pre_st_move and st_move == 1:
+                data_st = 1
+                calc_st = 0
+            elif 1 == pre_st_move and 0 == st_move:
+                data_st = 0
+                calc_st = 1
+                
+            if 1 == data_st:
+                #print "start:", gx, gy, gz
+                t,v =  calc_speed(pry[3])
+                d_vel_tm_buf.append([t,v])
+                print "tm:", t, "vel:", v, "v_acc:", pry[3]-1
+                #fd2.write('%03f %03f %03f %03f %03f %03f %03f %03f\n' % (t, pry[3], ax, ay, az, gx, gy, gz))  
+                
+            elif 1 == calc_st:
+                
+                #print "end:", gx, gy, gz
+                l_vel = []
+                speed = 0
+                pre_tm = 0.0
+                sum_t = 0.0
+                a = d_vel_tm_buf[-1][1]/ d_vel_tm_buf[-1][0]
+                #print "lean", a
+                last_tm = 0.0
+                fh = open("res.txt", "a")
+                for val in d_vel_tm_buf:            
+                    #l_vel.append([val[0] - last_tm , val[1] - val[0]*a])
+                    dis += (val[1] - val[0]*a)*(val[0] - last_tm)
+                    fh.write('%03f %03f %03f \n' % (val[0], val[1], val[1] - val[0]*a )) 
+                    last_tm = val[0]  
+                
+                
+                d_vel_tm_buf.clear()
+                fh.close()
+                print "height:",dis
+                dis = 0.0
+                calc_st = 0
+                data_st = 0
+                    
             #sum_vcc += pry[3]
             #print "vcc:", sum_vcc/sample_cnt, sample_cnt
             
@@ -237,14 +282,14 @@ def calc_task():
             #spd_array.append(spd)
             
             #print t, h, v, pry[3]
-            fh.write('%03f %03f %03f %03f\n' % (t, h, v, pry[3])) 
+            #fh.write('%03f %03f %03f %03f\n' % (t, h, v, pry[3])) 
             #fh.write('{:03f,:03f}\n'.format(ax,ay))
        
             
             
         else:
             time.sleep(0.01)
-    fh.close()
+    fd2.close()
     
 def uart_task():
     global ser
