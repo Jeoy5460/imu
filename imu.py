39488b84fde68d91dc54a72be7fd13c6e90e68cb
zhouyu5460 <zhouyu5460@gmail.com>
2015-07-31 15:07:01 +0800

    no more new

--- imu.py
+++ imu.py
@@ -78,6 +78,4 @@ def imu(acc,gyro):
   roll   = math.atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3
   yaw    = -math.atan2(2 * q1 * q2 + 2 * q0 * q3, -2 * q2*q2 - 2 * q3 * q3 + 1)* 57.3
   
-  
-  
   return (pitch, roll, yaw, v_acc)
