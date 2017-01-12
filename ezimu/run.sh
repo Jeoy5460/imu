44f4aad8c5a98ee211f99febe0ec9e5443293575
zhouyu <zhouyu5460@gmail.com>
2017-01-12 14:16:06 +0800

    added sync log

---
+++ ezimu/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+#rm -rf filt.dat
+#rm -rf up.dat
+#./test_run.py "-fup1.dat"
+#./plot_imu.py
+#./test_run.py "-b"  
+#./ezulp.py "68:C9:0B:06:BA:8B" "-S"  
+#./ezulp.py "D2:34:33:14:6F:D1" "-S"  
+#./ezulp.py "E4:0E:51:95:04:35" "-S"
+./ezulp.py "EE:95:8F:FE:82:D9" "-S"  
--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
-rm -rf filt.dat
-rm -rf up.dat
+#rm -rf filt.dat
+#rm -rf up.dat
 #./test_run.py "-fup1.dat"
 #./plot_imu.py
-./test_run.py "-b"  
+#./test_run.py "-b"  
+#./ezulp.py "EE:95:8F:FE:82:D9" "-S"  
+#sudo gatttool -b "E4:0E:51:95:04:35" -I -t random
+#sudo gatttool -b "EE:95:8F:FE:82:D9" -I -t random
+sudo gatttool -I -b "EE:95:8F:FE:82:D9" -t random
+
